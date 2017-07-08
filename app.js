cbc7d805e29c5ab9d9b36e13903dbbce7028f30a
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-08 17:14:29 +0000

    Listing 4.11 The next version of your static file app (app.js)

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 var express = require("express");
+var morgan = require("morgan");
 var path = require("path");
 var fs = require("fs");
 
 var app = express();
 
-app.use(function(req, res, next){
-console.log("Request IP: " + req.url);
-console.log("Request date: " + new Date());
-next();
-});
+app.use(morgan("short"));
 
-app.use(function(req, res, next){
-    var filePath = path.join(__dirname, "static", req.url);
-    fs.stat(filePath, function(err, fileInfo){
-        if(err){
-            next();
-            return;
-        }
-        if(fileInfo.isFile()){
-            res.sendFile(filePath);
-        } else {
-            next();
-        }
-    })
-});
+var staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
 
 app.use(function(req, res){
     res.status(404);
